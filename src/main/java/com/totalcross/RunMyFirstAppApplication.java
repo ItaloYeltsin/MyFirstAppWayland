999cae6e338a32c5818a0721dd82fe841b962344
italo yeltsin <br.yeltsin@gmail.com>
2019-12-04 18:16:58 -0300

    first commit

---
+++ src/main/java/com/totalcross/RunMyFirstAppApplication.java
@@ -0,0 +1,9 @@
+package com.totalcross;
+
+import totalcross.TotalCrossApplication;
+
+public class RunMyFirstAppApplication {
+    public static void main(String [] args) {
+        TotalCrossApplication.run(MyFirstApp.class, "/r", "5443444B3018A93884C5D622");
+    }
+}
